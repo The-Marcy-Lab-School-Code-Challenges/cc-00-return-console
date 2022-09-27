2fce5e4742bad6c70eb5ca0f938b934d9a68cbc4
Itzel01 <71834676+Itzel01@users.noreply.github.com>
2022-09-27 09:28:11 -0400

    cc-00-functions

--- index.js
+++ index.js
@@ -1,3 +1,27 @@
-function nameOfFunction(){
+//1
+function morningLogged(){
   
 }
+
+//2
+function iLoveCode(){
+  
+}
+
+//3
+function loggingTwice(){
+  
+}
+
+//4
+function noStep(){
+  
+}
+
+//5
+function isPrime(){
+  
+}
+
+
+
